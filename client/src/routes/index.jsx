906705be2a015a4ecbf51654c9b63a9cff4b7b6c
Teran Keith <terankeith33@gmail.com>
2019-02-28 07:14:26 -0600

    clean up unused files

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,13 +1,9 @@
 import Pages from "layouts/Pages.jsx";
-import RTL from "layouts/RTL.jsx";
-import Dashboard from "layouts/Dashboard.jsx";
 import Profile from "layouts/Profile.jsx";
 
 var indexRoutes = [
-  { path: "/rtl", name: "RTL", component: RTL },
-  { path: "/pages", name: "Pages", component: Pages },
-  { path: "/", name: "Home", component: Profile },
-  { path: "/dashboard", name: "Dashboard", component: Dashboard}
+    { path: "/pages", name: "Pages", component: Pages },
+    { path: "/", name: "Home", component: Profile }
 ];
 
 export default indexRoutes;
